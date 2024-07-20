5a097e0c2addceb1ad9da4f582b9a19c2e7685f3
Bobr <cyxov04@gmail.com>
2024-07-21 00:17:07 +0300

    somes

---
+++ app/src/main/java/com/github/bobryanskiy/tamagotchiforlovers/DifficultChooserScreen.kt
@@ -0,0 +1,35 @@
+package com.github.bobryanskiy.tamagotchiforlovers
+
+import android.app.ActivityOptions
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DifficultChooserScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_difficult_chooser_screen)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun con() {
+    }
+
+    fun goBack(view : View) {
+        finish()
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(0, 0)
+    }
+}
--- app/src/main/java/com/github/bobryanskiy/tamagotchiforlovers/GameChooseScreen.kt
+++ app/src/main/java/com/github/bobryanskiy/tamagotchiforlovers/GameChooseScreen.kt
@@ -1,9 +1,10 @@
 package com.github.bobryanskiy.tamagotchiforlovers
 
+import android.app.ActivityOptions
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.window.OnBackInvokedDispatcher
 
 class GameChooseScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +12,19 @@ class GameChooseScreen : AppCompatActivity() {
         setContentView(R.layout.activity_game_choose_screen)
     }
 
-    fun exitGame(view : View) {
+    fun goBack(view : View) {
         finish()
+    }
+
+    lateinit var PET_NAME : String
+
+    fun continueChoose(view : View) {
+        if (R.id.editTextInput.toString() != "") {
+            PET_NAME = R.id.editTextInput.toString()
+            val intent = Intent(this, DifficultChooserScreen::class.java)
+            val options = ActivityOptions.makeCustomAnimation(this, 0, 0)
+            startActivity(intent, options.toBundle())
+        }
     }
 
     override fun finish() {
