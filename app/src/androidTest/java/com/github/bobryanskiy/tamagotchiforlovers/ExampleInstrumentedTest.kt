743694851283ab1934a58121a819dc8a315a9a1a
Bobryanskiy <cyxov04@gmail.com>
2025-03-06 01:47:50 +0300

    ci/cd test

--- app/src/androidTest/java/com/github/bobryanskiy/tamagotchiforlovers/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/github/bobryanskiy/tamagotchiforlovers/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.tamagotchiforlovers", appContext.packageName)
+        assertEquals("com.github.bobryanskiy.tamagotchiforlovers", appContext.packageName)
     }
 }
