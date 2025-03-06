7eaa72ce21988096e1418fc502baadd0b9c67231
Bobryanskiy <cyxov04@gmail.com>
2025-03-06 13:16:55 +0300

    ci/cd test

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,6 +24,9 @@ android {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
+        debug {
+            signingConfig = signingConfigs.getByName("debug")
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
