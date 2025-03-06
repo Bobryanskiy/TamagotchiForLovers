1c5d8e3a9ea886893073579230ccebe1092094e1
Bobryanskiy <cyxov04@gmail.com>
2025-03-06 19:01:07 +0300

    ci/cd test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,3 @@
-buildscript {
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.6.1")
-        classpath("org.bouncycastle:bcprov-jdk15on:1.70")
-    }
-}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     id("com.android.application") version "8.6.1" apply false
